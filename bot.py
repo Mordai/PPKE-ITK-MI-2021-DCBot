4ca88119b5e72f6e596cd9921cd46bd0902c6df1
Lengyel Balázs <lengyel.balazs12@gmail.com>
2021-09-25 22:44:53 +0200

    First bot.py mechanics

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 #Main Workflow
+
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.environ.get('DC_TOKEN')
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print("Ready to go!")
+
+
+bot.run(TOKEN)
+
