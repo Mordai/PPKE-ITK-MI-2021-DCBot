3b5c7739e918495af8e93e72c86cd91560181ac3
Lengyel Balázs <lengyel.balazs12@gmail.com>
2021-09-26 18:04:00 +0200

    Bot load event added

--- bot.py
+++ bot.py
@@ -4,9 +4,14 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
+#Load/run nessesary components/functions
 load_dotenv()
 
+ #General variables
+now = datetime.now()
+MAIN_SERVER_GUILD = os.getenv("ITK_SERVER_ID")
 TOKEN = os.getenv("DC_TOKEN")
 
 bot = discord.Bot()
@@ -14,6 +19,8 @@ bot = discord.Bot()
 @bot.event
 async def on_ready():
     print("Ready to go!")
+    bot.CH_bot_log = bot.get_channel(891715602442510386)
+    bot.CH_bot_log.send(f"Bot started: ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 bot.run(TOKEN)
