232fbb35d192dd4c77b60575f5894277413a95b9
Lengyel Balázs <lengyel.balazs12@gmail.com>
2021-09-26 23:24:01 +0200

    Initialization message component finished

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 import subprocess
+from github import Github
+import git
 import pytz
 
 
@@ -14,6 +16,13 @@ import emojis
 
 #Load/run nessesary components/functions
 load_dotenv()
+g = Github(os.getenv("GITHUB_TOKEN"))
+repo = g.get_repo("Mordai/PPKE-ITK-MI-2021-DCBot")
+commits = repo.get_commits()
+last_commit = commits[0]
+
+local_repo = git.Repo(search_parent_directories=True)
+sha = local_repo.head.object.hexsha
 
 #General variables
 tz = pytz.timezone('Europe/Budapest')
@@ -25,13 +34,14 @@ TOKEN = os.getenv("DC_TOKEN")
 
 bot = discord.Bot()
 
+
 @bot.event
 async def on_ready():
     print("Ready to go!")
     bot.CH_bot_log = bot.get_channel(891715602442510386)
     message_log_start = f"{emojis.Emojis.StatusEmojis.sparkle} `Bot started: " + now.strftime("%Y-%m-%d %H:%M:%S") + "`"
     try:
-        message_local = "\n```Current HEAD → " + subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip() + \
+        message_local = "\n```Current HEAD → " + sha + \
         "\nCurrent author → " + subprocess.check_output(['git', 'config', '--global', 'user.name']).decode('UTF-8') + "```"
     except:
         message_local = ""
@@ -39,7 +49,10 @@ async def on_ready():
     try:
         message_heroku = "\n```Current HEAD → " + os.getenv("HEROKU_SLUG_COMMIT") + \
         "\nCurrent author → HEROKU deployment" + \
-        "\nCommit desc → " + os.getenv("HEROKU_SLUG_DESCRIPTION")
+        "\nCommit name → " + last_commit.commit.message.split('\n\n')[0] + \
+        "\nCommit desc → "  + last_commit.commit.message.split('\n\n')[1:] + \
+        "\nHeroku slug desc → " + os.getenv("HEROKU_SLUG_DESCRIPTION") + "```"
+
     except:
         message_heroku = ""
 
